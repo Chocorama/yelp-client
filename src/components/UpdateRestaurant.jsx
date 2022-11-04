b536938e1c4644d745b057c78bc87e31fe2dd6f9
Willem Humphrey <willemhumphrey@gmail.com>
2022-11-04 16:42:32 -0500

    add query to single restaurant and JSX in return statement

--- src/components/UpdateRestaurant.jsx
+++ src/components/UpdateRestaurant.jsx
@@ -1,25 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import RestaurantFinder from "../api/RestaurantFinder";
 
 const UpdateRestaurant = (props) => {
   const { id } = useParams();
+
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [priceRange, setPriceRange] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const response = await RestaurantFinder.get(`/${id}`);
+      console.log(response.data.data.restaurant);
+      setName(response.data.data.restaurant.name);
+      setLocation(response.data.data.restaurant.location);
+      setPriceRange(response.data.data.restaurant.price_range);
+    })();
+  }, [id]);
 
   return (
     <div>
       <form action="">
         <div className="form-group">
           <label htmlFor="name">Name</label>
-          <input id="name" className="form-control" type="text" />
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            id="name"
+            className="form-control"
+            type="text"
+          />
         </div>
 
         <div className="form-group">
           <label htmlFor="location">Location</label>
-          <input id="location" className="form-control" type="text" />
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            className="form-control"
+            type="text"
+          />
         </div>
 
         <div className="form-group">
           <label htmlFor="price_range">Price Range</label>
-          <input id="price_range" className="form-control" type="number" />
+          <input
+            value={priceRange}
+            onChange={(e) => setPriceRange(e.target.value)}
+            id="price_range"
+            className="form-control"
+            type="number"
+          />
         </div>
         <button className="btn btn-primary">submit</button>
       </form>

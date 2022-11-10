99a27802e11fa087413ca49ea16b53313209353e
Willem Humphrey <willemhumphrey@gmail.com>
2022-11-10 10:32:30 -0500

    finish reviews component

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import StarRating from "./StarRating";
 
 const Reviews = () => {
-  return <div>Reviews</div>;
+  return (
+    <div className="row row-cols-3 mb-2">
+      <div
+        style={{ maxWidth: "30%" }}
+        className="card text-white bg-primary mb-3 mr-4"
+      >
+        <div className="card-header d-flex justify-content-between">
+          <span>Joan</span>
+          <span>
+            <StarRating rating={3} />
+          </span>
+        </div>
+        <div className="card-body">
+          <p className="card-text">This place was awesome</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;

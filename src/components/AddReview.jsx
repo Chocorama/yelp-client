5eb6fcc3734fcba69bf80772185227adc47aa6e2
Willem Humphrey <willemhumphrey@gmail.com>
2022-12-21 14:52:46 -0500

    add state for addReview component

--- src/components/AddReview.jsx
+++ src/components/AddReview.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddReview = () => {
+  // const [name, setName] = useState("");
+  // const [reviewText, setReviewText] = useState("");
+  // const [rating, setRating] = useState("Rating");
+
+  const [reviewForm, setReviewForm] = useState({
+    name: "",
+    reviewText: "",
+    rating: "",
+  });
+
   return (
     <div className="mb-2">
       <form action="">
@@ -8,6 +18,10 @@ const AddReview = () => {
           <div className="form-group col-8">
             <label htmlFor="name">Name</label>
             <input
+              value={reviewForm.name}
+              onChange={(e) =>
+                setReviewForm({ ...reviewForm, name: e.target.value })
+              }
               placeholder="name"
               id="name"
               type="text"
@@ -16,7 +30,14 @@ const AddReview = () => {
           </div>
           <div className="form-group col-4">
             <label htmlFor="rating">rating</label>
-            <select id="rating" className="custom-select">
+            <select
+              value={reviewForm.rating}
+              onChange={(e) =>
+                setReviewForm({ ...reviewForm, rating: e.target.value })
+              }
+              id="rating"
+              className="custom-select"
+            >
               <option disabled>Rating</option>
               <option value="1">1</option>
               <option value="2">2</option>
@@ -29,6 +50,10 @@ const AddReview = () => {
           <label htmlFor="review">review</label>
           <textarea
             className="form-control"
+            value={reviewForm.reviewText}
+            onChange={(e) =>
+              setReviewForm({ ...reviewForm, reviewText: e.target.value })
+            }
             id="review"
             cols="30"
             rows="10"

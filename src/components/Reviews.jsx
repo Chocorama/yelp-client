b809ecaba4997908adbe2a4901507ccdbaac3340
Willem <willemhumphrey@gmail.com>
2023-01-17 15:48:24 -0800

    add rating destructure

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -6,7 +6,7 @@ const Reviews = ({ reviews }) => {
 
   return (
     <div className="row row-cols-3 mb-2">
-      {reviews.map(({ id, name, review }) => (
+      {reviews.map(({ id, name, review, rating }) => (
         <div
           key={id}
           style={{ maxWidth: "30%" }}
@@ -15,7 +15,7 @@ const Reviews = ({ reviews }) => {
           <div className="card-header d-flex justify-content-between">
             <span>{name}</span>
             <span>
-              <StarRating rating={3} />
+              <StarRating rating={rating} />
             </span>
           </div>
           <div className="card-body">

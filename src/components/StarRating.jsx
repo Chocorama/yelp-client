0db8a8beb355773b128bb1a2bfe65384a502a27d
Willem <willemhumphrey@gmail.com>
2023-01-28 12:12:02 -0800

    fix star-rating warning for key prop

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -9,7 +9,7 @@ const StarRating = ({ rating }) => {
     } else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
       console.log(Math.ceil(rating));
       stars.push(
-        <i className="fa-regular fa-star-half-stroke text-warning"></i>
+        <i key={i} className="fa-regular fa-star-half-stroke text-warning"></i>
       );
     } else if (i <= rating) {
       stars.push(<i key={i} className="fa-solid fa-star text-warning"></i>);

faf2996efba68898976dc892a9055eb105dddab5
Willem <willemhumphrey@gmail.com>
2023-01-28 12:43:30 -0800

    change layout of reviews size to 1x1

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import StarRating from "./StarRating";
 
-// TODO: change layout of reviews to 1x1 column with smaller max-width
-
 const Reviews = ({ reviews }) => {
   return (
-    <div className="row row-cols-3 mb-2">
+    <div className="row row-cols-1 mb-2">
       {reviews.map(({ id, name, review, rating }) => (
         <div key={id} style={{ minWidth: "240px" }} className="container">
-          <div className="row">
+          <div className="row mt-2">
             <div className="card bg-primary text-white">
               <div className="card-header">
                 <span>{name}</span>
